51879cba80cd266ab12b6675632f1a453085a34b
gauravm-30 <mauryagaurav550@gmail.com>
2024-05-14 00:17:15 +0530

    Multithreading basics

---
+++ MultithreadingPart1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    //Thread Based Multitasking
+    //Allows parts of same program to run concurrently on  the computer
+
+    //Threads vs Process
+    //Two threads share the same memory address space
+    //Context switching between threads is usually less expensive than between processes;
+    //The cost of communication between two threads is relatively low 
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
